fe8c49f1d32723a2ed145d87d8e009b1b732190b
ynottony <97442619+Nanogy98@users.noreply.github.com>
2023-05-29 18:14:02 +0900

    Update news_search.js

--- news_search.js
+++ news_search.js
@@ -1,4 +1,18 @@
- try {
+  data: {
+    name: "newssearch",
+    description: "ニュースを検索します。",
+    options: [
+      {
+        type: "STRING",
+        name: "word",
+        description: "入力しよう",
+        max_length: 90,
+        required: true,
+      },
+    ],
+  }, 
+
+try {
      const word = interaction.options.getString("word");
      const data = await fetch(`https://newsapi.org/v2/everything?q=${word}&apiKey=5d2e9a391cb44145a42c6a61151512ae`).then((res) => res.json()).catch(() => {});
      const embed = new Discord.MessageEmbed().addFields([{
