e4aed9359e63104a8aff4d048e98cb58a764cc08
ynottony <97442619+Nanogy98@users.noreply.github.com>
2023-05-29 18:14:28 +0900

    Update news_search.js

--- news_search.js
+++ news_search.js
@@ -14,7 +14,7 @@
 
 try {
      const word = interaction.options.getString("word");
-     const data = await fetch(`https://newsapi.org/v2/everything?q=${word}&apiKey=5d2e9a391cb44145a42c6a61151512ae`).then((res) => res.json()).catch(() => {});
+     const data = await fetch(`https://newsapi.org/v2/everything?q=${word}&apiKey=${process.env.news}`).then((res) => res.json()).catch(() => {});
      const embed = new Discord.MessageEmbed().addFields([{
        name: data.articles[0].title,
        value: data.articles[0].url + "\n" + data.articles[0].description
